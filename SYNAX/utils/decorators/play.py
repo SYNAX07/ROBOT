e2b423387938400723e6d03b009614c12e7db84b
Ashwani <162049637+SYNAX07@users.noreply.github.com>
2024-04-01 18:52:11 +0530

    Update play.py

--- SYNAX/utils/decorators/play.py
+++ SYNAX/utils/decorators/play.py
@@ -37,7 +37,7 @@ def PlayWrapper(command):
                     [
                         InlineKeyboardButton(
                             text="ʜᴏᴡ ᴛᴏ ғɪx ?",
-                            callback_data="DAXXmousAdmin",
+                            callback_data="cybersphinx",
                         ),
                     ]
                 ]
