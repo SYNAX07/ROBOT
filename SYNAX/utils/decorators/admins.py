2362c609bc25643fb19e0692e72c5fdcdd3aeb5b
Ashwani <162049637+SYNAX07@users.noreply.github.com>
2024-04-01 18:53:13 +0530

    Update admins.py

--- SYNAX/utils/decorators/admins.py
+++ SYNAX/utils/decorators/admins.py
@@ -44,7 +44,7 @@ def AdminRightsCheck(mystic):
                     [
                         InlineKeyboardButton(
                             text="ʜᴏᴡ ᴛᴏ ғɪx ?",
-                            callback_data="DAXXmousAdmin",
+                            callback_data="cybersphinx",
                         ),
                     ]
                 ]
@@ -140,7 +140,7 @@ def AdminActual(mystic):
                     [
                         InlineKeyboardButton(
                             text="ʜᴏᴡ ᴛᴏ ғɪx ?",
-                            callback_data="DAXXmousAdmin",
+                            callback_data="cybersphinx",
                         ),
                     ]
                 ]
