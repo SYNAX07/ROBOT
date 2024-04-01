7588a89150511c5e52ae1db390aa8eb95aa1d0d4
Ashwani <162049637+SYNAX07@users.noreply.github.com>
2024-04-01 18:26:14 +0530

    Add files via upload

---
+++ SYNAX/__init__.py
@@ -0,0 +1,28 @@
+from SYNAX.core.bot import DAXX
+from SYNAX.core.dir import dirr
+from SYNAX.core.git import git
+from SYNAX.core.userbot import Userbot
+from SYNAX.misc import dbb, heroku
+
+from SafoneAPI import SafoneAPI
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = DAXX()
+api = SafoneAPI()
+userbot = Userbot()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
---
+++ SYNAX/__main__.py
@@ -0,0 +1,62 @@
+import asyncio
+import importlib
+
+from pyrogram import idle
+from pytgcalls.exceptions import NoActiveGroupCall
+
+import config
+from SYNAX import LOGGER, app, userbot
+from SYNAX.core.call import DAXX
+from SYNAX.misc import sudo
+from SYNAX.plugins import ALL_MODULES
+from SYNAX.utils.database import get_banned_users, get_gbanned
+from config import BANNED_USERS
+
+
+async def init():
+    if (
+        not config.STRING1
+        and not config.STRING2
+        and not config.STRING3
+        and not config.STRING4
+        and not config.STRING5
+    ):
+        LOGGER(__name__).error("𝐒𝐭𝐫𝐢𝐧𝐠 𝐒𝐞𝐬𝐬𝐢𝐨𝐧 𝐍𝐨𝐭 𝐅𝐢𝐥𝐥𝐞𝐝, 𝐏𝐥𝐞𝐚𝐬𝐞 𝐅𝐢𝐥𝐥 𝐀 𝐏𝐲𝐫𝐨𝐠𝐫𝐚𝐦 𝐒𝐞𝐬𝐬𝐢𝐨𝐧")
+        exit()
+    await sudo()
+    try:
+        users = await get_gbanned()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+        users = await get_banned_users()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+    except:
+        pass
+    await app.start()
+    for all_module in ALL_MODULES:
+        importlib.import_module("SYNAX.plugins" + all_module)
+    LOGGER("SYNAX.plugins").info("𝐀𝐥𝐥 𝐅𝐞𝐚𝐭𝐮𝐫𝐞𝐬 𝐋𝐨𝐚𝐝𝐞𝐝 𝐁𝐚𝐛𝐲🥳...")
+    await userbot.start()
+    await DAXX.start()
+    try:
+        await DAXX.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
+    except NoActiveGroupCall:
+        LOGGER("SYNAX").error(
+            "𝗣𝗹𝗭 𝗦𝗧𝗔𝗥𝗧 𝗬𝗢𝗨𝗥 𝗟𝗢𝗚 𝗚𝗥𝗢𝗨𝗣 𝗩𝗢𝗜𝗖𝗘𝗖𝗛𝗔𝗧\𝗖𝗛𝗔𝗡𝗡𝗘𝗟\n\nSYNAX 𝗕𝗢𝗧 𝗦𝗧𝗢𝗣........"
+        )
+        exit()
+    except:
+        pass
+    await DAXX.decorators()
+    LOGGER("SYNAX").info(
+        "╔═════ஜ۩۞۩ஜ════╗\n  ☠︎︎𝗠𝗔𝗗𝗘 𝗕𝗬 𝗠𝗥 SYNAX☠︎︎\n╚═════ஜ۩۞۩ஜ════╝"
+    )
+    await idle()
+    await app.stop()
+    await userbot.stop()
+    LOGGER("SYNAX").info("𝗦𝗧𝗢𝗣 SYNAX 𝗠𝗨𝗦𝗜𝗖🎻 𝗕𝗢𝗧..")
+
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(init())
