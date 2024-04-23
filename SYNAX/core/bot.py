1e55c225d771b4a0db3c97eb95476462764507ab
Ashwani <162049637+SYNAX07@users.noreply.github.com>
2024-04-23 09:51:43 +0530

    Update bot.py

--- SYNAX/core/bot.py
+++ SYNAX/core/bot.py
@@ -6,7 +6,7 @@ import config
 from ..logging import LOGGER
 
 
-class DAXX(Client):
+class SYNAX(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
